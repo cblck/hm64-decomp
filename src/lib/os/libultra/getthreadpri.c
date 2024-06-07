f52adc78bf582e6fd0625f7f12a15bb6e8321982
cblck <156492894+cblck@users.noreply.github.com>
2024-06-07 01:39:04 -0700

    ASM to C: osGetThreadPri. (Coped from: n64decomp/perfect_dark/src/lib/ultra/os/getthreadpri.c)

--- src/lib/os/libultra/getthreadpri.c
+++ src/lib/os/libultra/getthreadpri.c
@@ -1,4 +1,13 @@
 #include "common.h"
 
+extern OSThread* __osRunningThread;
 
-INCLUDE_ASM(const s32, "lib/os/libultra/getthreadpri", osGetThreadPri);
+//INCLUDE_ASM(const s32, "lib/os/libultra/getthreadpri", osGetThreadPri);
+
+OSPri osGetThreadPri(OSThread* thread) {
+	if (thread == NULL) {
+		thread = __osRunningThread;
+	}
+
+	return thread->priority;
+}
