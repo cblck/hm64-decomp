bdbdf8deae7b377cc642b63e4e48116a086b89d5
cblck <156492894+cblck@users.noreply.github.com>
2024-06-07 12:26:01 -0700

    ASM to C: osCreateMesgQueue. (n64decomp/perfect_dark/src/lib/ultra/os/createmesgqueue.c)

--- src/lib/os/libultra/createmesgqueue.c
+++ src/lib/os/libultra/createmesgqueue.c
@@ -1,4 +1,14 @@
 #include "common.h"
 
+extern struct __osThreadTail __osThreadTail;
 
-INCLUDE_ASM(const s32, "lib/os/libultra/createmesgqueue", osCreateMesgQueue);
+//INCLUDE_ASM(const s32, "lib/os/libultra/createmesgqueue", osCreateMesgQueue);
+
+void osCreateMesgQueue(OSMesgQueue* mq, OSMesg* msg, s32 msgCount) {
+	mq->mtqueue = (OSThread*)&__osThreadTail;
+	mq->fullqueue = (OSThread*)&__osThreadTail;
+	mq->validCount = 0;
+	mq->first = 0;
+	mq->msgCount = msgCount;
+	mq->msg = msg;
+}
