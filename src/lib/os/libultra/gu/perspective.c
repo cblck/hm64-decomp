3bf52b589f1cc92cc3183a85b8223f290ac46c9a
cblck <156492894+cblck@users.noreply.github.com>
2024-06-17 17:20:41 -0700

    Updating libultra code to match decompals/ultralib. Renamed sins.c to sinf.c. Adjusted sinf.rodata split.

--- src/lib/os/libultra/gu/perspective.c
+++ src/lib/os/libultra/gu/perspective.c
@@ -1,7 +1,6 @@
 #include "common.h"
 
+
 INCLUDE_ASM(const s32, "lib/os/libultra/gu/perspective", guPerspectiveF);
 
 INCLUDE_ASM(const s32, "lib/os/libultra/gu/perspective", guPerspective);
-
-INCLUDE_RODATA(const s32, "lib/os/libultra/gu/perspective", perspective_rodata_0038);
