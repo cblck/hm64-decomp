2f4384649325f1d1183e8986eb1a25c2332d5fcf
cblck <156492894+cblck@users.noreply.github.com>
2024-06-19 00:36:44 -0700

    Adds split for lib/os/libultra/libc/xlitob rodata [ldigs, udigs].

--- src/lib/os/libultra/libc/xlitob.c
+++ src/lib/os/libultra/libc/xlitob.c
@@ -2,3 +2,7 @@
 
 
 INCLUDE_ASM(const s32, "lib/os/libultra/libc/xlitob", _Litob);
+
+INCLUDE_RODATA(const s32, "lib/os/libultra/libc/xlitob", ldigs);
+
+INCLUDE_RODATA(const s32, "lib/os/libultra/libc/xlitob", udigs);
