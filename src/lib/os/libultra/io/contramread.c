53702345494dea4b266098f5f85ea1e83f18865a
cblck <156492894+cblck@users.noreply.github.com>
2024-06-19 00:59:52 -0700

    Adjust contramread split.

--- src/lib/os/libultra/io/contramread.c
+++ src/lib/os/libultra/io/contramread.c
@@ -1,3 +1,6 @@
 #include "common.h"
 
+
 INCLUDE_ASM(const s32, "lib/os/libultra/io/contramread", __osContRamRead);
+
+INCLUDE_RODATA(const s32, "lib/os/libultra/io/contramread", __osPfsLastChannel);
