490ab4ae0950fee9fd5eb96995aaacf645246f86
cblck <156492894+cblck@users.noreply.github.com>
2024-09-18 21:15:46 -0700

    Update nuaustlbankset.c; moves nualstl.h to headers; updates mus_x_bank addresses.

--- src/lib/nusys-1/nuaustlbankset.c
+++ src/lib/nusys-1/nuaustlbankset.c
@@ -1,14 +1,29 @@
+/*======================================================================*/
+/*		NuSYS										*/
+/*		nuaustlbankset.c								*/
+/*												*/
+/*		Copyright (C) 1997, NINTENDO Co,Ltd.				*/
+/*												*/
+/*======================================================================*/
 #include <nusys.h>
-#include "nualstl/nualstl.h"
+#include <nualstl.h>
 
 extern ptr_bank_t	*mus_init_bank;
 extern ptr_bank_t	*mus_default_bank;
 
-//INCLUDE_ASM(const s32, "lib/nusys-1/nuaustlbankset", nuAuStlBankSet);
+/*----------------------------------------------------------------------------*/
+/*	nuAuStlBankSet - Set sound bank							*/
+/*	IN:	pbk_addr	ROM address of pointer that refers to wave table	*/
+/*		pbk_size	size of pointer that refers to wave table			*/
+/*		wbk_addr	ROM address of wave table data				*/
+/*	RET:	None											*/
+/*----------------------------------------------------------------------------*/
+void nuAuStlBankSet(u8 *pbk_addr, u32 pbk_size, u8 *wbk_addr)
+{
 
-void nuAuStlBankSet(u8 *pbk_addr, u32 pbk_size, u8 *wbk_addr) {
     nuAuSeqBank_ptr = alHeapAlloc(&nuAuHeap, 1, pbk_size);
     nuPiReadRom((u32)pbk_addr, nuAuSeqBank_ptr, pbk_size);
+
     MusBankInitialize(nuAuSeqBank_ptr, wbk_addr);
-    mus_init_bank = mus_default_bank = (ptr_bank_t *)nuAuSeqBank_ptr;
+    mus_default_bank = mus_init_bank = (ptr_bank_t *)nuAuSeqBank_ptr;
 }
