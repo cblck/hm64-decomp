b181a4c5307e32ceb3ca2164f1aed7166ead04f8
cblck <156492894+cblck@users.noreply.github.com>
2024-09-19 22:57:08 -0700

    Updated gettime.c. Moved __osCurrentTime down 0x4 from 0x8018A08C to 0x8018A088.

--- src/lib/os/libultra/os/gettime.c
+++ src/lib/os/libultra/os/gettime.c
@@ -1,4 +1,25 @@
-#include "common.h"
+#include "PR/os_internal.h"
+#include "PR/ultraerror.h"
+#include "PRinternal/osint.h"
+#include "PRinternal/viint.h"
 
+OSTime osGetTime() {
+	u32 tmptime;
+	u32 elapseCount;
+	OSTime currentCount;
+	register u32 saveMask;
 
-INCLUDE_ASM(const s32, "lib/os/libultra/os/gettime", osGetTime);
+#ifdef _DEBUG
+	if (!__osViDevMgr.active) {
+		__osError(ERR_OSGETTIME, 0);
+		return 0;
+	}
+#endif
+
+	saveMask = __osDisableInt();
+	tmptime = osGetCount();
+	elapseCount = tmptime - __osBaseCounter;
+	currentCount = __osCurrentTime;
+	__osRestoreInt(saveMask);
+	return currentCount + elapseCount;
+}
