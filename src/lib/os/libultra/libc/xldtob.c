0988a27a9a0852f26158c222ee77309a1e2fdb9e
cblck <156492894+cblck@users.noreply.github.com>
2024-09-20 13:49:19 -0700

    Moves and renames files to match ultralib.

---
+++ src/lib/os/libultra/libc/xldtob.c
@@ -0,0 +1,16 @@
+#include "common.h"
+
+
+INCLUDE_RODATA(const s32, "lib/os/libultra/libc/xldtob", xldtob_rodata_0000);
+
+INCLUDE_RODATA(const s32, "lib/os/libultra/libc/xldtob", xldtob_rodata_0048);
+
+INCLUDE_RODATA(const s32, "lib/os/libultra/libc/xldtob", xldtob_rodata_004C);
+
+INCLUDE_ASM(const s32, "lib/os/libultra/libc/xldtob", _Ldtob);
+
+INCLUDE_ASM(const s32, "lib/os/libultra/libc/xldtob", _Ldunscale);
+
+INCLUDE_ASM(const s32, "lib/os/libultra/libc/xldtob", _Genld);
+
+INCLUDE_RODATA(const s32, "lib/os/libultra/libc/xldtob", xldtob_rodata_0060);
