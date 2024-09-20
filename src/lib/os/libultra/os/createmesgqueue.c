6e50c64ff3dc57742771c32cba07bcc97a0a777f
cblck <156492894+cblck@users.noreply.github.com>
2024-09-19 21:16:00 -0700

    Update createmesgqueue.c.

--- src/lib/os/libultra/os/createmesgqueue.c
+++ src/lib/os/libultra/os/createmesgqueue.c
@@ -1,4 +1,20 @@
-#include "common.h"
+#include "PR/os_internal.h"
+#include "PR/ultraerror.h"
+#include "PRinternal/osint.h"
 
+void osCreateMesgQueue(OSMesgQueue* mq, OSMesg* msg, s32 msgCount) {
 
-INCLUDE_ASM(const s32, "lib/os/libultra/os/createmesgqueue", osCreateMesgQueue);
+#ifdef _DEBUG
+	if (msgCount <= 0) {
+		__osError(ERR_OSCREATEMESGQUEUE, 1, msgCount);
+		return;
+	}
+#endif
+
+	mq->mtqueue = (OSThread*)&__osThreadTail.next;
+	mq->fullqueue = (OSThread*)&__osThreadTail.next;
+	mq->validCount = 0;
+	mq->first = 0;
+	mq->msgCount = msgCount;
+	mq->msg = msg;
+}
