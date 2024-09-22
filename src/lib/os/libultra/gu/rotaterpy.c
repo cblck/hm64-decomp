ee0dbe03ed1d933b870d10f173f0051397ce5aa2
cblck <156492894+cblck@users.noreply.github.com>
2024-09-21 23:42:27 -0700

    Renames rotate.c to rotaterpy.c.

---
+++ src/lib/os/libultra/gu/rotaterpy.c
@@ -0,0 +1,8 @@
+#include "common.h"
+
+
+INCLUDE_ASM(const s32, "lib/os/libultra/gu/rotaterpy", guRotateRPYF);
+
+INCLUDE_ASM(const s32, "lib/os/libultra/gu/rotaterpy", guRotateRPY);
+
+INCLUDE_RODATA(const s32, "lib/os/libultra/gu/rotaterpy", rotaterpy_data_0000);
