f48b472dd80c3a729751a204e71cf3972418187c
cblck <156492894+cblck@users.noreply.github.com>
2024-10-04 00:46:32 -0700

    Updates and renames n_adpcm.c to n_load.c.

--- src/lib/os/libultra/libnaudio/n_adpcm.c
+++
@@ -1,7 +0,0 @@
-#include "common.h"
-
-INCLUDE_ASM(const s32, "lib/os/libultra/libnaudio/n_adpcm", n_alAdpcmPull);
-
-INCLUDE_ASM(const s32, "lib/os/libultra/libnaudio/n_adpcm", n_alLoadParam);
-
-INCLUDE_ASM(const s32, "lib/os/libultra/libnaudio/n_adpcm", _n_decodeChunk);
