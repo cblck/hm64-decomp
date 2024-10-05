31586006a9d56d8ac7e37537c3c0f66a71ebb14d
cblck <156492894+cblck@users.noreply.github.com>
2024-10-05 16:53:04 -0700

    Updates rmonbrk.c. Adds dbgdefs.h and dbgproto.h.

--- src/lib/os/libultra/rmon/rmonbrk.c
+++ src/lib/os/libultra/rmon/rmonbrk.c
@@ -1,4 +1,38 @@
 #include "common.h"
+////////////////////////////////////////////////
+#include "PR/os_internal.h"
+#include "dbgproto.h"
+#include "PR/rcp.h"
+#include "PR/sptask.h"
+#include "PRinternal/rmonint.h"
+
+#include "macros.h"
+
+// TODO: this comes from a header
+#if BUILD_VERSION >= VERSION_J
+#ident "$Revision: 1.4 $"
+#else
+#ident "$Revision: 3.70 $"
+#ident "$Revision: 1.5 $"
+#ident "$Revision: 1.2 $"
+#ident "$Revision: 1.4 $"
+#ident "$Revision: 1.3 $"
+#endif
+
+#define TMP_BP 0
+#define NUM_BREAKPOINTS 16
+
+typedef struct {
+	TVushort type;
+	TVushort response;
+	TVid threadID;
+	void* pc;
+} TVExceptionReplyMsg;
+
+typedef struct {
+	u32* breakAddress;
+	u32 oldInstruction;
+} BREAKINFO;
 
 INCLUDE_ASM(const s32, "lib/os/libultra/rmon/rmonbrk", __rmonSetBreak);
 
