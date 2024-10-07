c134be24674d45f4c25f7f416a6b9cdd17c88074
cblck <156492894+cblck@users.noreply.github.com>
2024-10-06 23:24:01 -0700

    Updates nupakmenuloadfont.c with TODO to resolve need to extern the noncontiguous bss/common symbols.

--- src/lib/nusys-1/nupakmenuloadfont.c
+++ src/lib/nusys-1/nupakmenuloadfont.c
@@ -52,7 +52,7 @@ s8 font_kana_dot[] = {	/* font pattern of DAKUTEN,HANDAKUTEN	*/
 };
 #endif /* LANG_JAPANESE */
 
-extern u16	*__font_buf;
+extern u16	*__font_buf;	// TODO: Fix extern
 
 /*----------------------------------------------------------------------*/
 /*	nuPakMenuLoadFont - Loads and deploys fonts			*/
@@ -65,10 +65,162 @@ extern u16	*__font_buf;
 /*				Japanese	4768byte(NU_PAKMENU_FONTSIZE_J)	*/
 /*	RTN:	nothing							*/
 /*----------------------------------------------------------------------*/
-INCLUDE_ASM(const s32, "lib/nusys-1/nupakmenuloadfont", nuPakMenuLoadFont);
+void nuPakMenuLoadFont(u16 *font_buf)
+{
+    register u16	*fpix;
+    register u32	*farc = (u32 *)FONTARC_TOP+1;
+    register u32	bitmap = *(farc-1);
+    register u32	position = 0;
+    register int	i;
+    register int	j;
+    int	k;
 
-INCLUDE_ASM(const s32, "lib/nusys-1/nupakmenuloadfont", canvas_u8_to_str);
+    __font_buf = font_buf;
+    
+    /* Clear font buffer	*/
+    for (i = 0; i < FONT_HT * (FONT_CODE_BTM - FONT_CODE_TOP + 2); i++){
+	*font_buf++ = 0x0000;
+    }
+  
+    /*  Extract font		*/
+    for (i = 0; i < sizeof(canvas_ftbl)/sizeof(FTBL); i ++){
+#ifdef LANG_JAPANESE
+	if (i == 3){
+	    farc = (u32 *)font_bitmaps;
+	    position  = 0;
+	    bitmap = *farc ++;
+	}
+#endif
+	
+	fpix = __font_buf + (canvas_ftbl[i].code * FONT_HT + FONT_HOFS);
+	for (j = 0; j < canvas_ftbl[i].number; j ++){
+	    for (k = 0; k < FONTARC_HT; k ++){
+		position += FONTARC_WD;
+		if (position & ~0x1f) {
+		    position &= 0x1f;
+		    *fpix = (u16)(bitmap << position)  & 0x1fff;
+		    bitmap = *farc ++;
+		    if (!position) {
+			goto nosLoadFont_00;
+		    }
+		}
+		*fpix |= (u16)(bitmap >> (32 - position)) & 0x1fff;
+	nosLoadFont_00:
+		fpix ++;
+		
+#if   	DEBUG_FONT
+		{
+		    int n;
+		    osSyncPrintf("j=%3d k=%3d position=%3d  ", j, k, position);
+		    for (n = 15; n >= 0; n --){
+			if (*(fpix-1) & (1 << n)) osSyncPrintf("O");
+			else			    osSyncPrintf("-");
+		    }
+		    osSyncPrintf("\n");
+		}
+#endif
+		
+	    }
+	    fpix += (FONT_HT-FONTARC_HT);
+	    position += 2;
+	}
+    }
+    
+#if	LANG_JAPANESE
+    // put DAKUTEN,HANDAKUTEN
+	   for (i = F_ADRS(KA); i < F_ADRS(MA); i ++) {
+	       (__font_buf+(F_ADRS(GA)-F_ADRS(KA)))[i] = __font_buf[i];
+	   }
+    for (i = F_ADRS(HA); i < F_ADRS(MA); i ++) {
+	(__font_buf+(F_ADRS(BA)-F_ADRS(HA)))[i] = __font_buf[i];
+    }  
+    
+    k = 0;
+    fpix = __font_buf + F_ADRS(GA);
+    for (i = FONT_CODE_GA; i < FONT_CODE_PA+5; i ++){
+	if (i == FONT_CODE_PA) k = 8;
+	for (j = 0; j < 4; j ++){
+	    fpix[j] = (fpix[j] | font_kana_dot[j+k]) & font_kana_dot[j+k+4];
+	}
+	
+	
+#if	DEBUG_FONT
+	{
+	    int	n;
+	    for (j = 0; j < FONT_HT; j ++){
+		for (n = 15; n >= 0; n --){
+		    if (fpix[j] & (1 << n)) osSyncPrintf("O");
+		    else			    osSyncPrintf("-");
+		}
+		osSyncPrintf("\n");
+	    }
+	    osSyncPrintf("\n");
+	}
+#endif
+	fpix += FONT_HT;
+    }
+#endif
+}
 
-INCLUDE_ASM(const s32, "lib/nusys-1/nupakmenuloadfont", canvas_putc);
+void	canvas_u8_to_str(u16 n, u8 *str)
+{
+    if (n > 999) {
+	n = 999;
+    }
+    
+#if 1
+    str[0] = n / 100 + 0x0f + (n >= 100);
+    str[1] = (n % 100) / 10  + 0x0f + (n >= 10);
+    str[2] = n % 10  + 0x10;
+#else
+    static  u8  ofs[] = { 0x0f, 0x0f, 0x10, 0x10, 0x10 };
+    s32	i;
 
-INCLUDE_ASM(const s32, "lib/nusys-1/nupakmenuloadfont", canvas_puts);
+    if      (n >= 100) i = 2;
+    else if (n >= 10)  i = 1;
+    else               i = 0;
+    
+    str[0] = n / 100 + ofs[i+0];
+    n %= 100;  
+    str[1] = n / 10  + ofs[i+1];
+    str[2] = n % 10  + ofs[i+2];
+#endif
+}  
+
+extern u16	*__cursor;	// TODO: Fix extern
+
+void	canvas_putc(int c, u16 color_fg, u16 color_bg)
+{
+    register int	i;
+    register int	j;
+    register u16	*fpix;
+    register u16	*curp;
+
+    if (c == FONT_CODE_QSKIP) {
+	__cursor += FONT_WSKIP/3;
+    } else {
+	if (c < FONT_CODE_TOP || FONT_CODE_BTM < c) {
+	    c = FONT_CODE_NON;
+	}
+	fpix = __font_buf + (c - FONT_CODE_TOP)*FONT_HT;
+	curp = __cursor;
+	
+	for (i = 0; i < FONT_HT; i ++) {
+	    for (j = FONT_WD - 1; j >= 0; j --) {
+		if ((*fpix >> j) & 1){
+		    *(curp)             = color_fg;
+		    *(curp+1+SCREEN_WD) = color_bg;
+		}
+		curp ++;
+	    }
+	    curp += (SCREEN_WD - FONT_WD);
+	    fpix ++;
+	}
+	__cursor += FONT_WSKIP;
+    }
+}
+
+void	canvas_puts(u8 *str, int n, u16 color_fg, u16 color_bg)
+{
+    while (n -- && *str) canvas_putc(*str ++, color_fg, color_bg);
+}
