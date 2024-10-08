ef5850a9be5f8ef0f9be54996284b3e0833e8c9f
cblck <156492894+cblck@users.noreply.github.com>
2024-10-07 18:10:30 -0700

    Renames nuaumgr.c to nuaustlmgr.c and updated symbols.

---
+++ src/lib/nusys-1/nuaustlmgr.c
@@ -0,0 +1,11 @@
+#include "common.h"
+
+INCLUDE_ASM(const s32, "lib/nusys-1/nuaustlmgr", auDmaCallBack);
+
+INCLUDE_ASM(const s32, "lib/nusys-1/nuaustlmgr", auDmaNew);
+
+INCLUDE_ASM(const s32, "lib/nusys-1/nuaustlmgr", auCleanDMABuffers);
+
+INCLUDE_ASM(const s32, "lib/nusys-1/nuaustlmgr", nuAuStlMgrInit);
+
+INCLUDE_ASM(const s32, "lib/nusys-1/nuaustlmgr", nuAuMgr);
