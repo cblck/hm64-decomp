161bd1423fb5e43e140109a991a3597368db8d01
cblck <156492894+cblck@users.noreply.github.com>
2024-10-11 15:52:47 -0700

    Updates nucontqueryread.c.

--- src/lib/nusys-1/nucontqueryread.c
+++ src/lib/nusys-1/nucontqueryread.c
@@ -1,4 +1,42 @@
-#include "common.h"
+/*======================================================================*/
+/*		NuSYS										*/
+/*		nucontqueryread.c								*/
+/*												*/
+/*		Copyright (C) 1997, NINTENDO Co,Ltd.				*/
+/*												*/
+/*----------------------------------------------------------------------*/    
+/* Ver 1.0	97/10/9		Created by Kensaku Ohki(SLANP)		*/
+/*======================================================================*/
+/* $Id: nucontqueryread.c,v 1.4 1997/12/06 08:46:47 ohki Exp $		*/
+/*======================================================================*/
+#include <nusys.h>
 
-// nuContQueryRead
-INCLUDE_ASM(const s32, "lib/nusys-1/nucontqueryread", func_800FC650);
+
+/*----------------------------------------------------------------------*/
+/*	nuContQueryRead - Get the state of the controller 			*/
+/*	IN:	None 										*/
+/*	RET:	None 										*/
+/*----------------------------------------------------------------------*/
+void nuContQueryRead(void)
+{
+
+    NUContQueryMesg	queryMesg;
+    OSMesgQueue		rtnMesgQ;
+    OSMesg		rtnMesgBuf;
+
+    if(nuContCallBack.func == NULL){
+#ifdef NU_DEBUG    
+	osSyncPrintf("nuContQueryRead: no controller manager!!\n");
+#endif /* NU_DEBUG */
+	return;
+    }
+
+    osCreateMesgQueue(&rtnMesgQ, &rtnMesgBuf, 1);
+
+    queryMesg.mesg	= NU_CONT_QUERY_MSG;
+    queryMesg.rtnMesgQ  = &rtnMesgQ;
+    
+    osSendMesg(&nuSiMgrMesgQ, (OSMesg*)&queryMesg, OS_MESG_BLOCK);
+    osRecvMesg(&rtnMesgQ, NULL, OS_MESG_BLOCK);
+}
+    
