c7634e7eee62aeaf027cf0e4cd5230cca13eb096
cblck <156492894+cblck@users.noreply.github.com>
2024-10-11 01:01:06 -0700

    Updates nusicallbackremove.c.

--- src/lib/nusys-1/nusicallbackremove.c
+++ src/lib/nusys-1/nusicallbackremove.c
@@ -1,4 +1,44 @@
-#include "common.h"
+/*======================================================================*/
+/*		NuSYS										*/
+/*		nusicallbackremove.c							*/
+/*												*/
+/*		Copyright (C) 1997, NINTENDO Co,Ltd.				*/
+/*												*/
+/*----------------------------------------------------------------------*/
+/* Ver 1.1	97/12/3		Created by Kensaku Ohki(SLANP)		*/
+/*======================================================================*/
+/* $Id: nusicallbackremove.c,v 1.2 1997/12/05 12:36:41 ohki Exp $		*/
+/*======================================================================*/
+#include <nusys.h>
+/*----------------------------------------------------------------------*/
+/* nuSiCallBackRemove - Delete call back function from SI manager   	*/
+/* Deletes call back function allocated to call back function list from */
+/* list.  If not allocated, it does nothing.  		    			*/
+/*	IN:	*list    Pointer of call back function list structure		*/
+/*	RTN:	none										*/
+/*----------------------------------------------------------------------*/
+void nuSiCallBackRemove(NUCallBackList* list)
+{
+    OSIntMask		mask;
+    NUCallBackList*	siCallBackListPtr;
+    NUCallBackList*	forwardPtr;
 
-// nuSiCallBackRemove
-INCLUDE_ASM(const s32, "lib/nusys-1/nusicallbackremove", func_800FD900);
+
+    /* Look for the function in the list*/
+    siCallBackListPtr = nuSiCallBackList.next;
+    forwardPtr = &nuSiCallBackList;
+
+    while(siCallBackListPtr){
+
+	if(siCallBackListPtr->func == list->func){
+	    mask = osSetIntMask(OS_IM_NONE);
+	    forwardPtr->next = list->next;
+	    list->next = NULL;
+	    list->func = NULL;
+	    osSetIntMask(mask);
+	    break;
+	}
+	forwardPtr = siCallBackListPtr;
+	siCallBackListPtr = siCallBackListPtr->next;
+    }
+}
