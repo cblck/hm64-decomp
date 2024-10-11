328ae75c05ee1eae6db8025f4fc5d39712cb91b7
cblck <156492894+cblck@users.noreply.github.com>
2024-10-11 00:57:54 -0700

    Updates nusicallbackadd.c.

--- src/lib/nusys-1/nusicallbackadd.c
+++ src/lib/nusys-1/nusicallbackadd.c
@@ -1,4 +1,46 @@
-#include "common.h"
+/*======================================================================*/
+/*		NuSYS										*/
+/*		nusicallbackadd.c								*/
+/*												*/
+/*		Copyright (C) 1997, NINTENDO Co,Ltd.				*/
+/*												*/
+/*----------------------------------------------------------------------*/
+/* Ver 1.1	97/12/1		Created by Kensaku Ohki(SLANP)		*/
+/*======================================================================*/
+/* $Id: nusicallbackadd.c,v 1.4 1997/12/05 12:59:57 ohki Exp $		*/
+/*======================================================================*/
+#include <nusys.h>
 
-// nuSiCallBackAdd
-INCLUDE_ASM(const s32, "lib/nusys-1/nusicallbackadd", func_800FD880);
+/*----------------------------------------------------------------------*/
+/* nuSiCallBackAdd - Allocate a call back function to SI manager 		*/
+/*	IN:	*list	pointer of call back function list structure  		*/
+/* 		*func	call back function pointer					*/
+/*	RTN:none										*/
+/*----------------------------------------------------------------------*/
+void nuSiCallBackAdd(NUCallBackList* list, NUCallBackFunc func)
+{
+    OSIntMask		mask;
+    NUCallBackList*	siCallBackListPtr;
+    NUCallBackList*	LastPtr;
+
+    /* Look for the end of the list*/
+    siCallBackListPtr = &nuSiCallBackList;
+    while(siCallBackListPtr){
+
+	/* If same function is already allocated, it does not allocate it.   */
+	if(siCallBackListPtr->func == func){
+#ifdef NU_DEBUG
+	    osSyncPrintf("nuSiCallBackAdd: CallBack is already added!!\n");
+#endif /* NU_DEBUG */
+	    return;
+	}
+	LastPtr = siCallBackListPtr;
+	siCallBackListPtr = siCallBackListPtr->next;
+    }
+    
+    mask = osSetIntMask(OS_IM_NONE);
+    list->next = LastPtr->next;
+    list->func = func;
+    LastPtr->next = list;
+    osSetIntMask(mask);
+}
