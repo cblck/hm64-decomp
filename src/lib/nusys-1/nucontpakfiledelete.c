6f66ba1b8ba60818faa17835da58d212111035d0
cblck <156492894+cblck@users.noreply.github.com>
2024-10-11 21:47:39 -0700

    Updates nucontpakfiledelete.c.

--- src/lib/nusys-1/nucontpakfiledelete.c
+++ src/lib/nusys-1/nucontpakfiledelete.c
@@ -1,4 +1,45 @@
-#include "common.h"
+/*======================================================================*/
+/*		NuSYS										*/
+/*		nucontpakfiledelete.c							*/
+/*												*/
+/*		Copyright (C) 1997, NINTENDO Co,Ltd.				*/
+/*												*/
+/*----------------------------------------------------------------------*/    
+/* Ver 1.0	97/10/9		Created by Kensaku Ohki(SLANP)		*/
+/*======================================================================*/
+/* $Id: nucontpakfiledelete.c,v 1.2 1997/12/05 13:03:08 ohki Exp $	*/
+/*======================================================================*/
+#include <nusys.h>
 
-// nuContPakFileDelete
-INCLUDE_ASM(const s32, "lib/nusys-1/nucontpakfiledelete", func_800FCD00);
+/*----------------------------------------------------------------------*/
+/*	nuContPakFileDelete - Delete the controller pack note 		*/
+/* 	Open the file of the controller pack 					*/
+/*	IN:	*file	The controller pack structure 				*/
+/*		noteName 	The note name 						*/
+/*		extName		The extension  					*/
+/*	RET:	None 										*/
+/*----------------------------------------------------------------------*/
+void nuContPakFileDelete(NUContPakFile *file, u8* noteName, u8* extName)
+{
+    OSMesgQueue		rtnMesgQ;
+    OSMesg		rtnMesgBuf;
+    NUContPakFileOpenMesg fopenMesg;
+    
+
+    if(nuContPakCallBack.func == NULL){
+#ifdef NU_DEBUG
+	osSyncPrintf("nuContPakFileDelete: no contpak manager!!\n");
+#endif /* NU_DEBUG */
+	return;
+    }
+	
+    osCreateMesgQueue(&rtnMesgQ, &rtnMesgBuf, 1);
+    
+    fopenMesg.mesg     = NU_CONT_PAK_FILEDELETE_MSG;
+    fopenMesg.file     = file;
+    fopenMesg.rtnMesgQ = &rtnMesgQ;
+    fopenMesg.noteName = noteName;
+    fopenMesg.extName  = extName;
+    osSendMesg(&nuSiMgrMesgQ, (OSMesg*)&fopenMesg, OS_MESG_BLOCK);
+    osRecvMesg(&rtnMesgQ, NULL, OS_MESG_BLOCK);
+}
