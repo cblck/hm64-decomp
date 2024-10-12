7a472700ce16f384968937fa1a9eb22457c3cc06
cblck <156492894+cblck@users.noreply.github.com>
2024-10-11 22:35:28 -0700

    Updates nugfxswapcfbfuncset.c.

--- src/lib/nusys-1/nugfxswapcfbfuncset.c
+++ src/lib/nusys-1/nugfxswapcfbfuncset.c
@@ -1,4 +1,25 @@
-#include "common.h"
+/*======================================================================*/
+/*		NuSYS										*/
+/*		nugfxswapcfbfuncset.c							*/
+/*												*/
+/*		Copyright (C) 1997, NINTENDO Co,Ltd.				*/
+/*												*/
+/*----------------------------------------------------------------------*/    
+/* Ver 1.0	97/10/9		Created by Kensaku Ohki(SLANP)		*/
+/*======================================================================*/
+#include <nusys.h>
 
+/*----------------------------------------------------------------------*/
+/*	Register the call-back function for swithcing frame buffers 	*/
+/*	IN:	The pointer for the main function 					*/
+/*	RET:	None 										*/
+/*----------------------------------------------------------------------*/
+void nuGfxSwapCfbFuncSet(NUGfxSwapCfbFunc func)
+{
+    OSIntMask mask;
+    
+    mask = osSetIntMask(OS_IM_NONE);
+    nuGfxSwapCfbFunc = func;
+    osSetIntMask(mask);
+}
 
-INCLUDE_ASM(const s32, "lib/nusys-1/nugfxswapcfbfuncset", nuGfxSwapCfbFuncSet);
