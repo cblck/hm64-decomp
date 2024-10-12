0bde5b579487825e2447ebf4c3b40232ff7072ef
cblck <156492894+cblck@users.noreply.github.com>
2024-10-11 22:05:56 -0700

    Updates nugfxdisplayon.c.

--- src/lib/nusys-1/nugfxdisplayon.c
+++ src/lib/nusys-1/nugfxdisplayon.c
@@ -1,3 +1,20 @@
-#include "common.h"
+/*======================================================================*/
+/*		NuSYS										*/
+/*		nugfxdisplayon.c								*/
+/*												*/
+/*		Copyright (C) 1997, NINTENDO Co,Ltd.				*/
+/*												*/
+/*----------------------------------------------------------------------*/    
+/* Ver 1.0	97/10/9		Created by Kensaku Ohki(SLANP)		*/
+/*======================================================================*/
+#include <nusys.h>
 
-INCLUDE_ASM(const s32, "lib/nusys-1/nugfxdisplayon", nuGfxDisplayOn);
+/*----------------------------------------------------------------------*/
+/*	DISPLAY-ON										*/
+/*	IN:	None										*/
+/*	RET:	None										*/
+/*----------------------------------------------------------------------*/
+void nuGfxDisplayOn(void)		
+{
+    nuGfxDisplay = NU_GFX_DISPLAY_ON;
+}
