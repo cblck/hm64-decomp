a3a0b376375e64a2e7ac7669b2d376ccfa9e0eba
cblck <156492894+cblck@users.noreply.github.com>
2024-10-11 19:56:51 -0700

    Updates nucontdatalock.c.

--- src/lib/nusys-1/nucontdatalock.c
+++ src/lib/nusys-1/nucontdatalock.c
@@ -1,7 +1,38 @@
-#include "common.h"
+/*======================================================================*/
+/*		NuSYS										*/
+/*		nucondatalock.c								*/
+/*												*/
+/*		Copyright (C) 1997, NINTENDO Co,Ltd.				*/
+/*												*/
+/*----------------------------------------------------------------------*/    
+/* Ver 1.0	97/10/9		Created by Kensaku Ohki(SLANP)		*/
+/*======================================================================*/
+#include <nusys.h>
 
-// nuContDataLock
-INCLUDE_ASM(const s32, "lib/nusys-1/nucontdatalock", func_800FC440);
+/*----------------------------------------------------------------------*/
+/*	nuContDataLock - Lock controller data 					*/
+/*	IN:	None 										*/
+/*	RET:	None 										*/
+/*----------------------------------------------------------------------*/
+void nuContDataLock(void)
+{
+    OSIntMask	mask;
 
-// nuContDataUnLock
-INCLUDE_ASM(const s32, "lib/nusys-1/nucontdatalock", func_800FC470);
+    mask = osSetIntMask(OS_IM_NONE);
+    nuContDataLockKey = NU_CONT_DATA_LOCK;
+    osSetIntMask(mask);
+
+}
+/*----------------------------------------------------------------------*/
+/*	nuContDataUnLock - Unlock controller data 				*/
+/*	IN:	None 										*/
+/*	RET:	None 										*/
+/*----------------------------------------------------------------------*/
+void nuContDataUnLock(void)
+{
+    OSIntMask	mask;
+    
+    mask = osSetIntMask(OS_IM_NONE);
+    nuContDataLockKey = NU_CONT_DATA_UNLOCK;
+    osSetIntMask(mask);
+}    
