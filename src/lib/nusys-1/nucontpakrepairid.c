ed157573eb6a88a73d5d5719e2e627e3012d7a50
cblck <156492894+cblck@users.noreply.github.com>
2024-10-11 23:20:15 -0700

    Updates nucontpakrepairid.c.

--- src/lib/nusys-1/nucontpakrepairid.c
+++ src/lib/nusys-1/nucontpakrepairid.c
@@ -1,3 +1,39 @@
-#include "common.h"
+/*======================================================================*/
+/*		NuSYS										*/
+/*		nucontpakrepairid.c							*/
+/*												*/
+/*		Copyright (C) 1997, NINTENDO Co,Ltd.				*/
+/*												*/
+/*----------------------------------------------------------------------*/    
+/* Ver 1.0	97/10/9		Created by Kensaku Ohki(SLANP)		*/
+/*======================================================================*/
+/* $Id: nucontpakrepairid.c,v 1.3 1997/12/05 13:03:49 ohki Exp $		*/
+/*======================================================================*/
+#include <nusys.h>
 
-INCLUDE_ASM(const s32, "lib/nusys-1/nucontdataread", nuContPakRepairId);
+/*----------------------------------------------------------------------*/
+/*	nuContPakRepairId - Repair the file system of the controller pack */
+/*	Use it if PFS_ERR_ID_FATAL returns in osPfsInitPak. 			*/
+/*	IN:	*file	The controller pack structure 				*/
+/*	RET:	None 										*/
+/*----------------------------------------------------------------------*/
+void nuContPakRepairId(NUContPakFile *file)
+{
+    OSMesgQueue		rtnMesgQ;
+    OSMesg		rtnMesgBuf;
+    NUContPakMesg 	repairMesg;
+
+    if(nuContPakCallBack.func == NULL){
+#ifdef NU_DEBUG
+	osSyncPrintf("nuContPakRepairId: no contpak manager!!\n");
+#endif /* NU_DEBUG */
+	return;
+    }
+    
+    osCreateMesgQueue(&rtnMesgQ, &rtnMesgBuf, 1);
+    repairMesg.mesg       = NU_CONT_PAK_REPAIRID_MSG;
+    repairMesg.file       = file;
+    repairMesg.rtnMesgQ   = &rtnMesgQ;
+    osSendMesg(&nuSiMgrMesgQ, (OSMesg*)&repairMesg, OS_MESG_BLOCK);
+    osRecvMesg(&rtnMesgQ, NULL, OS_MESG_BLOCK);
+}
