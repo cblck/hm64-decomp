b74c2bddd4a6e28994ce019bf979937bad642286
cblck <156492894+cblck@users.noreply.github.com>
2024-10-11 22:21:26 -0700

    Updates nugfxprenmifuncset.c.

--- src/lib/nusys-1/nugfxprenmifuncset.c
+++ src/lib/nusys-1/nugfxprenmifuncset.c
@@ -1,3 +1,24 @@
-#include "common.h"
+/*======================================================================*/
+/*		NuSYS										*/
+/*		nugfxprenmifuncset.c							*/
+/*												*/
+/*		Copyright (C) 1997, NINTENDO Co,Ltd.				*/
+/*												*/
+/*----------------------------------------------------------------------*/    
+/* Ver 1.0	97/10/9		Created by Kensaku Ohki(SLANP)		*/
+/*======================================================================*/
+#include <nusys.h>
 
-INCLUDE_ASM(const s32, "lib/nusys-1/nugfxprenmifuncset", nuGfxPreNMIFuncSet);
+/*----------------------------------------------------------------------------*/
+/*	Register the PRENMI call-back function in the loop of the Gfx thread	*/
+/*	IN:	The pointer of the call-back function 					*/
+/*	RET:	None 											*/
+/*----------------------------------------------------------------------------*/
+void nuGfxPreNMIFuncSet(NUGfxPreNMIFunc func)
+{
+    OSIntMask mask;
+
+    mask = osSetIntMask(OS_IM_NONE);
+    nuGfxPreNMIFunc = func;
+    osSetIntMask(mask);
+}
