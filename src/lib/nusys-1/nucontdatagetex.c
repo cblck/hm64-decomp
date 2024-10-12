5795b0814de2f71575313215246f6b012f75e1d5
cblck <156492894+cblck@users.noreply.github.com>
2024-10-11 19:51:16 -0700

    Updates nucontdatagetex.c and nucontdatagetexall.c.

--- src/lib/nusys-1/nucontdatagetex.c
+++ src/lib/nusys-1/nucontdatagetex.c
@@ -1,3 +1,33 @@
-#include "common.h"
+/*======================================================================*/
+/*		NuSYS										*/
+/*		nucontdatagetex.c								*/
+/*												*/
+/*		Copyright (C) 1997, NINTENDO Co,Ltd.				*/
+/*												*/
+/*----------------------------------------------------------------------*/    
+/* Ver 1.0	97/10/9		Created by Kensaku Ohki(SLANP)		*/
+/*======================================================================*/
+/* $Id: nucontdatagetex.c,v 1.2 1997/12/10 08:29:37 ohki Exp $		*/
+/*======================================================================*/
+#include <nusys.h>
 
-INCLUDE_ASM(const s32, "lib/nusys-1/nucontdatagetex", nuContDataGetEx);
+
+/*----------------------------------------------------------------------*/
+/*	nuContDataGetEx - Get the controller data 				*/
+/*  Read the extended controller data 						*/
+/*  Read with adding the trigger data to the OSContPad type of N64OS 	*/
+/*	IN:	*contpad  controller structure pointer of NUContData type 	*/
+/* 		padNo     The controller number 					*/
+/*----------------------------------------------------------------------*/
+void nuContDataGetEx(NUContData *contdata, u32 padno)
+{
+    u16	button;
+
+    button = contdata->button;
+    nuContDataClose();	/* data close */    
+    bcopy(&nuContData[padno], contdata, sizeof(OSContPad));
+
+    /* Create the trigger data */
+    contdata->trigger = nuContData[padno].button & (~button);
+    nuContDataOpen();	/* data open */
+}
--- src/lib/nusys-1/nucontdatagetexall.c
+++ src/lib/nusys-1/nucontdatagetexall.c
@@ -1,23 +1,35 @@
-#include "nusys.h"
+/*======================================================================*/
+/*		NuSYS										*/
+/*		nucontdatagetexall.c							*/
+/*												*/
+/*		Copyright (C) 1997, NINTENDO Co,Ltd.				*/
+/*												*/
+/*----------------------------------------------------------------------*/    
+/* Ver 1.0	97/10/9		Created by Kensaku Ohki(SLANP)		*/
+/*======================================================================*/
+/* $Id: nucontdatagetexall.c,v 1.2 1997/12/10 08:29:59 ohki Exp $		*/
+/*======================================================================*/
+#include <nusys.h>
 
-//INCLUDE_ASM(const s32, "lib/nusys-1/nucontdatagetexall", nuContDataGetExAll);
 
-void nuContDataGetExAll(NUContData *contdata) {
-
+/*----------------------------------------------------------------------*/
+/*	nuContDataGetExAll - Get controller data 					*/
+/*  Read extended controller data 							*/
+/*  Read with adding trigger data to the OSContPad type of N64OS 		*/
+/*	IN:	*contpad controller structure pointer of NUContData type 	*/
+/*----------------------------------------------------------------------*/
+void nuContDataGetExAll(NUContData *contdata)
+{
     u16	button;
     u32	cnt;
     
-    nuContDataClose(); 
- 
-    for (cnt = 0; cnt < NU_CONT_MAXCONTROLLERS; cnt++) {
- 
-        button = contdata[cnt].button;
-        bcopy(&nuContData[cnt], &contdata[cnt], sizeof(OSContPad));
-        
-        contdata[cnt].trigger = nuContData[cnt].button & (~button);
-    
+    nuContDataClose();	/* data close */    
+    for(cnt =0; cnt < NU_CONT_MAXCONTROLLERS; cnt++){
+	button = contdata[cnt].button;
+	bcopy(&nuContData[cnt], &contdata[cnt], sizeof(OSContPad));
+	
+	/* Create trigger data */
+	contdata[cnt].trigger = nuContData[cnt].button & (~button);
     }
-    
-    nuContDataOpen();	
-
+    nuContDataOpen();	/* data open */
 }
