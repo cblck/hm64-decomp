03e1b0981fa030fd4c1956dd8b70895a61b9055d
cblck <156492894+cblck@users.noreply.github.com>
2024-10-11 22:32:29 -0700

    Updates nugfxsetcfb.c.

--- src/lib/nusys-1/nugfxsetcfb.c
+++ src/lib/nusys-1/nugfxsetcfb.c
@@ -1,8 +1,23 @@
-#include "nusys.h"
+/*======================================================================*/
+/*		NuSYS										*/
+/*		nugfxsetcfb.c								*/
+/*												*/
+/*		Copyright (C) 1997, NINTENDO Co,Ltd.				*/
+/*												*/
+/*----------------------------------------------------------------------*/    
+/* Ver 1.0	97/10/9		Created by Kensaku Ohki(SLANP)		*/
+/*======================================================================*/  
+#include <nusys.h>
 
-void nuGfxSetCfb(u16** framebuf, u32 framebufnum) {
-    nuGfxCfb = framebuf;
-    nuGfxCfbNum = framebufnum;
+/*----------------------------------------------------------------------------*/
+/*	Set the frame buffer 									*/
+/*	IN:	**framebuf The pointer of the pointer array of the frame buffer	*/
+/*		framebufnum The number of frame buffers 					*/
+/*----------------------------------------------------------------------------*/
+void nuGfxSetCfb(u16** framebuf, u32 framebufnum)
+{
+    nuGfxCfb	= framebuf;
+    nuGfxCfbNum	= framebufnum;
     nuGfxCfbCounter = 0;
     nuGfxCfb_ptr = nuGfxCfb[0];
 }
