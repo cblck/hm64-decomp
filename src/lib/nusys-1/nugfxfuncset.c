e0d4abfcb56a001eb5f38b24759bc885f22fa570
cblck <156492894+cblck@users.noreply.github.com>
2024-10-11 22:16:53 -0700

    Updates nugfxfuncset.c.

--- src/lib/nusys-1/nugfxfuncset.c
+++ src/lib/nusys-1/nugfxfuncset.c
@@ -1,3 +1,25 @@
-#include "common.h"
+/*======================================================================*/
+/*		NuSYS										*/
+/*		nugfxfuncset.c								*/
+/*												*/
+/*		Copyright (C) 1997, NINTENDO Co,Ltd.				*/
+/*												*/
+/*----------------------------------------------------------------------*/    
+/* Ver 1.0	97/10/9		Created by Kensaku Ohki(SLANP)		*/
+/*======================================================================*/
+#include <nusys.h>
 
-INCLUDE_ASM(const s32, "lib/nusys-1/nugfxfuncset", nuGfxFuncSet);
+/*----------------------------------------------------------------------------*/
+/*	Register the retrace call-back function in the loop of the Gfx thread.	*/
+/*	IN:	The pointer of the call-back function 					*/
+/*	RET:	None 											*/
+/*----------------------------------------------------------------------------*/
+void nuGfxFuncSet(NUGfxFunc func)
+{
+    OSIntMask mask;
+    nuGfxTaskAllEndWait();			/* Wait for the task ends  */	    
+
+    mask = osSetIntMask(OS_IM_NONE);
+    nuGfxFunc = func;
+    osSetIntMask(mask);
+}
