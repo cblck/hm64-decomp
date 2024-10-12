ee03b2b09d355aae090718330bec1d61c660f400
cblck <156492894+cblck@users.noreply.github.com>
2024-10-12 15:30:24 -0700

    Updates nupireadrom.c with manually edited version (matches 1.6 but function is duplicated.)

--- src/lib/nusys-1/nupireadrom.c
+++ src/lib/nusys-1/nupireadrom.c
@@ -1,8 +1,118 @@
-#include "common.h"
+/*======================================================================*/
+/*		NuSYS										*/
+/*		nupireadrom.c								*/
+/*												*/
+/*		Copyright (C) 1997, NINTENDO Co,Ltd.				*/
+/*												*/
+/*----------------------------------------------------------------------*/
+/* Ver 1.0	97/10/9		Created by Kensaku Ohki(SLANP)		*/
+/*----------------------------------------------------------------------*/
+/* $Id: nupireadrom.c,v *Modified version of 1.6.* $		           	*/
+/*======================================================================*/
+#include <nusys.h>
 
-INCLUDE_ASM(const s32, "lib/nusys-1/nupireadrom", nuPiReadRom);
 
-// TODO or FIXME:
-// same as nuPiReadRom?
-// probably should be separate file?
-INCLUDE_ASM(const s32, "lib/nusys-1/nupireadrom", func_800FD690);
+
+/*----------------------------------------------------------------------*/
+/*	nuPiReadRom  - DMA-transfer the data from ROM 				*/
+/*	DMA-transfer the data from ROM. 						*/
+/*	The message queue is the local variable so that it can be used 	*/
+/*	between threads.									*/
+/*	Already changed to EPI	 							*/
+/*												*/
+/*	IN:	The head pointer of the heap buffer 				*/
+/*	RET:	None 										*/
+/*----------------------------------------------------------------------*/
+void nuPiReadRom(u32 rom_addr, void* buf_ptr, u32 size)
+{
+    OSIoMesg	dmaIoMesgBuf;
+    OSMesgQueue dmaMesgQ;
+    OSMesg	dmaMesgBuf;
+    OSPiHandle*	cartHandle;
+    
+#ifdef	USE_EPI
+    
+    cartHandle = osCartRomInit();
+
+    /* Create message queue */
+    osCreateMesgQueue(&dmaMesgQ, &dmaMesgBuf, 1);
+
+    /* Nullify the CPU cache */
+    osInvalDCache((void*)buf_ptr, (s32)size);
+
+    dmaIoMesgBuf.hdr.pri      = OS_MESG_PRI_NORMAL;
+    dmaIoMesgBuf.hdr.retQueue = &dmaMesgQ;
+    dmaIoMesgBuf.dramAddr     = buf_ptr;
+    dmaIoMesgBuf.devAddr      = (u32)rom_addr;
+    dmaIoMesgBuf.size         = size;
+
+    osEPiStartDma(cartHandle, &dmaIoMesgBuf, OS_READ);
+
+
+    /* End standby */
+    (void)osRecvMesg(&dmaMesgQ, &dmaMesgBuf, OS_MESG_BLOCK);
+    
+#else
+
+     /* Nullify the CPU cache */
+    osInvalDCache((void*)buf_ptr, (s32)size);
+
+    /* Create the message queue */
+    osCreateMesgQueue(&dmaMesgQ, &dmaMesgBuf, 1);
+    
+
+    /* Read DMA */
+    osPiStartDma(&dmaIoMesgBuf, OS_MESG_PRI_NORMAL, OS_READ, rom_addr,
+		 buf_ptr, size, &dmaMesgQ);
+
+    /* Wait for the end */
+    (void)osRecvMesg(&dmaMesgQ, NULL, OS_MESG_BLOCK);
+#endif /* USE_EPI */
+}
+
+void nuPiReadRom2(u32 rom_addr, void* buf_ptr, u32 size)
+{
+	OSIoMesg	dmaIoMesgBuf;
+	OSMesgQueue dmaMesgQ;
+	OSMesg	dmaMesgBuf;
+	OSPiHandle* cartHandle;
+
+#ifdef	USE_EPI
+
+	cartHandle = osCartRomInit();
+
+	/* Create message queue */
+	osCreateMesgQueue(&dmaMesgQ, &dmaMesgBuf, 1);
+
+	/* Nullify the CPU cache */
+	osInvalDCache((void*)buf_ptr, (s32)size);
+
+	dmaIoMesgBuf.hdr.pri = OS_MESG_PRI_NORMAL;
+	dmaIoMesgBuf.hdr.retQueue = &dmaMesgQ;
+	dmaIoMesgBuf.dramAddr = buf_ptr;
+	dmaIoMesgBuf.devAddr = (u32)rom_addr;
+	dmaIoMesgBuf.size = size;
+
+	osEPiStartDma(cartHandle, &dmaIoMesgBuf, OS_READ);
+
+
+	/* End standby */
+	(void)osRecvMesg(&dmaMesgQ, &dmaMesgBuf, OS_MESG_BLOCK);
+
+#else
+
+	/* Nullify the CPU cache */
+	osInvalDCache((void*)buf_ptr, (s32)size);
+
+	/* Create the message queue */
+	osCreateMesgQueue(&dmaMesgQ, &dmaMesgBuf, 1);
+
+
+	/* Read DMA */
+	osPiStartDma(&dmaIoMesgBuf, OS_MESG_PRI_NORMAL, OS_READ, rom_addr,
+		buf_ptr, size, &dmaMesgQ);
+
+	/* Wait for the end */
+	(void)osRecvMesg(&dmaMesgQ, NULL, OS_MESG_BLOCK);
+#endif /* USE_EPI */
+}
