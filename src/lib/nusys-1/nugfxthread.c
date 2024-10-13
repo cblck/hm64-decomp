cf5b0d95282b84776bf6b61428ee30fdfe6b31b8
cblck <156492894+cblck@users.noreply.github.com>
2024-10-13 00:24:13 -0700

    Updates nugfxthread.c with data and bss symbols (externs non-contiguous bss symbol nuGfxMesgQ.)

--- src/lib/nusys-1/nugfxthread.c
+++ src/lib/nusys-1/nugfxthread.c
@@ -1,6 +1,23 @@
 #include "common.h"
+/*======================================================================*/
+/*		NuSYS										*/
+/*		nugfxthread.c								*/
+/*												*/
+/*		Copyright (C) 1997, NINTENDO Co,Ltd.				*/
+/*												*/
+/*----------------------------------------------------------------------*/    
+/* Ver 1.0	97/10/9		Created by Kensaku Ohki(SLANP)		*/
+/*======================================================================*/
+#include <nusys.h>
+
+NUGfxFunc	nuGfxFunc = NULL;
+NUGfxPreNMIFunc	nuGfxPreNMIFunc = NULL;
+extern /*TODO: Fix extern.*/ OSMesgQueue	nuGfxMesgQ;
+
+static OSMesg	nuGfxMesgBuf[NU_GFX_MESGS];
+static OSThread	GfxThread;			/* graphic thread */
+static u64	GfxStack[NU_GFX_STACK_SIZE/8];	/* gfx thread stack  */
 
 INCLUDE_ASM(const s32, "lib/nusys-1/nugfxthread", nuGfxThreadStart);
 
-// nuGfxThreadStart
-INCLUDE_ASM(const s32, "lib/nusys-1/nugfxthread", func_800FB968);
+INCLUDE_ASM(const s32, "lib/nusys-1/nugfxthread", nuGfxThread);
