13573cc9acec92a3ca56c57a5764cecb7d8f4730
cblck <156492894+cblck@users.noreply.github.com>
2024-10-15 17:18:46 -0700

    Updates nuaustlplayerinit.c with manual edits; externs temp named symbols.

--- src/lib/nusys-1/nuaustlplayerinit.c
+++ src/lib/nusys-1/nuaustlplayerinit.c
@@ -1,3 +1,28 @@
-#include "common.h"
+/*======================================================================*/
+/*		NuSYS										*/
+/*		nuaustlplayerinit.c							*/
+/*												*/
+/*		Copyright (C) 1997, NINTENDO Co,Ltd.				*/
+/*												*/
+/*======================================================================*/
+/* Modified version to match HM64.                                      */
+#include <nusys.h>
+#include <nualstl.h>
 
-INCLUDE_ASM(const s32, "lib/nusys-1/nuaustlplayerinit", nuAuStlPlayerInit);
+extern void* D_8018965C; // TODO: Find real name.
+extern void* D_801897E0; // TODO: Find real name.
+
+/*----------------------------------------------------------------------*/
+/*	nuAuStlPlayerInit - Initialize sequence player				*/
+/*	IN:	*config	N64 Sound Tools player set structure		*/
+/*		size	    	buffer size to state sequence data			*/
+/*	RET:	None										*/
+/*----------------------------------------------------------------------*/
+void nuAuStlPlayerInit(musConfig *c, u32 size)
+{
+    MusInitialize(c);
+    nuAuSeqPlayer[NU_AU_SEQ_PLAYER0].data_ptr = alHeapAlloc(&nuAuHeap, 1, size);
+    nuAuSeqPlayer[NU_AU_SEQ_PLAYER1].data_ptr = alHeapAlloc(&nuAuHeap, 1, size); // HM64: Added this.
+	D_8018965C = alHeapAlloc(&nuAuHeap, 1, size); // HM64: Added this.
+	D_801897E0 = alHeapAlloc(&nuAuHeap, 1, size); // HM64: Added this.
+}
