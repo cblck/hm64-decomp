aa10c21a74bc460b872f74a12095bf06432cba3d
cblck <156492894+cblck@users.noreply.github.com>
2024-10-17 22:22:45 -0700

    Updates controller.c.

--- src/lib/os/libultra/io/controller.c
+++ src/lib/os/libultra/io/controller.c
@@ -1,9 +1,67 @@
 #include "common.h"
+//
+#include "macros.h"
+#include "PR/os_internal.h"
+#include "PRinternal/controller.h"
+#include "PRinternal/siint.h"
+
+OSPifRam __osContPifRam ALIGNED(16);
+u8 __osContLastCmd;
+u8 __osMaxControllers;
+
+OSTimer __osEepromTimer;
+OSMesgQueue __osEepromTimerQ ALIGNED(8);
+OSMesg __osEepromTimerMsg;
+
+s32 __osContinitialized = 0;
 
 INCLUDE_ASM(const s32, "lib/os/libultra/io/controller", osContInit);
 
-INCLUDE_ASM(const s32, "lib/os/libultra/io/controller", __osContGetInitData);
+void __osContGetInitData(u8* pattern, OSContStatus* data) {
+    u8* ptr;
+    __OSContRequesFormat requestHeader;
+    int i;
+    u8 bits = 0;
 
-INCLUDE_ASM(const s32, "lib/os/libultra/io/controller", __osPackRequestData);
+    ptr = (u8*)__osContPifRam.ramarray;
+    for (i = 0; i < __osMaxControllers; i++, ptr += sizeof(requestHeader), data++) {
+        requestHeader = *(__OSContRequesFormat*)ptr;
+        data->errno = CHNL_ERR(requestHeader);
 
-INCLUDE_RODATA(const s32, "lib/os/libultra/io/controller", controller_data_0000);
+        if (data->errno != 0) {
+            continue;
+        }
+
+        data->type = requestHeader.typel << 8 | requestHeader.typeh;
+        data->status = requestHeader.status;
+        bits |= 1 << i;
+    }
+    *pattern = bits;
+}
+
+void __osPackRequestData(u8 cmd) {
+    u8* ptr;
+    __OSContRequesFormat requestHeader;
+    s32 i;
+
+    for (i = 0; i < ARRLEN(__osContPifRam.ramarray); i++) {
+        __osContPifRam.ramarray[i] = 0;
+    }
+
+    __osContPifRam.pifstatus = CONT_CMD_EXE;
+    ptr = (u8*)__osContPifRam.ramarray;
+    requestHeader.dummy = CONT_CMD_NOP;
+    requestHeader.txsize = CONT_CMD_RESET_TX;
+    requestHeader.rxsize = CONT_CMD_RESET_RX;
+    requestHeader.cmd = cmd;
+    requestHeader.typeh = CONT_CMD_NOP;
+    requestHeader.typel = CONT_CMD_NOP;
+    requestHeader.status = CONT_CMD_NOP;
+    requestHeader.dummy1 = CONT_CMD_NOP;
+
+    for (i = 0; i < __osMaxControllers; i++) {
+        *(__OSContRequesFormat*)ptr = requestHeader;
+        ptr += sizeof(requestHeader);
+    }
+    *ptr = CONT_CMD_END;
+}
