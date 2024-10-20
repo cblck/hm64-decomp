c200eb12f347c7dd01f70dac20d3873f2220e5fa
cblck <156492894+cblck@users.noreply.github.com>
2024-10-19 23:08:51 -0700

    Renames rmonsio function __gdbIOflush to __rmonIOflush.

--- src/lib/os/libultra/rmon/rmonsio.c
+++ src/lib/os/libultra/rmon/rmonsio.c
@@ -3,7 +3,7 @@
 
 INCLUDE_ASM(const s32, "lib/os/libultra/rmon/rmonsio", __rmonSendFault);
 
-INCLUDE_ASM(const s32, "lib/os/libultra/rmon/rmonsio", __gdbIOflush);
+INCLUDE_ASM(const s32, "lib/os/libultra/rmon/rmonsio", __rmonIOflush);
 
 INCLUDE_ASM(const s32, "lib/os/libultra/rmon/rmonsio", __rmonIOputw);
 
