27ff080f7459b5c9e4d41c969374fd90e280f8cf
cblck <156492894+cblck@users.noreply.github.com>
2024-10-22 14:20:33 -0700

    Updates vi.c. splits.

--- src/lib/os/libultra/io/vi.c
+++ src/lib/os/libultra/io/vi.c
@@ -2,10 +2,8 @@
 
 INCLUDE_ASM(const s32, "lib/os/libultra/io/vi", __osViInit);
 
-INCLUDE_RODATA(const s32, "lib/os/libultra/io/vi", vi_data_0000);
+INCLUDE_RODATA(const s32, "lib/os/libultra/io/vi", vi);
 
-INCLUDE_RODATA(const s32, "lib/os/libultra/io/vi", vi_data_0004);
+INCLUDE_RODATA(const s32, "lib/os/libultra/io/vi", __osViCurr);
 
-INCLUDE_RODATA(const s32, "lib/os/libultra/io/vi", vi_data_0034);
-
-INCLUDE_RODATA(const s32, "lib/os/libultra/io/vi", vi_data_0038);
+INCLUDE_RODATA(const s32, "lib/os/libultra/io/vi", __osViNext);
