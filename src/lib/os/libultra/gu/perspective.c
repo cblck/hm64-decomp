b4aecf437ed122cd6cb3549cb34fcd26e3b2e556
cblck <156492894+cblck@users.noreply.github.com>
2024-10-22 00:05:01 -0700

    Adjusts perspective/sinf data split.

--- src/lib/os/libultra/gu/perspective.c
+++ src/lib/os/libultra/gu/perspective.c
@@ -3,5 +3,3 @@
 INCLUDE_ASM(const s32, "lib/os/libultra/gu/perspective", guPerspectiveF);
 
 INCLUDE_ASM(const s32, "lib/os/libultra/gu/perspective", guPerspective);
-
-INCLUDE_RODATA(const s32, "lib/os/libultra/gu/perspective", perspective_rodata_0038);
--- src/lib/os/libultra/gu/sinf.c
+++ src/lib/os/libultra/gu/sinf.c
@@ -1,4 +1,5 @@
 #include "common.h"
 
+INCLUDE_RODATA(const s32, "lib/os/libultra/gu/sinf", D_80123BD0);
 
 INCLUDE_ASM(const s32, "lib/os/libultra/gu/sinf", sinf);
