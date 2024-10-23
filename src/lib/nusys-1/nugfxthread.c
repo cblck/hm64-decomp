cea1b9b34f7054c5f4625e00b26fab4fada5bb18
cblck <156492894+cblck@users.noreply.github.com>
2024-10-23 16:33:32 -0700

    Updated nugfxthread.c with a modified version of 1.0. (Credit to Chippy and mono21400 for their help in matching nuGfxThread.)

--- src/lib/nusys-1/nugfxthread.c
+++ src/lib/nusys-1/nugfxthread.c
@@ -1,24 +1,80 @@
-#include "common.h"
 /*======================================================================*/
-/*		NuSYS										*/
-/*		nugfxthread.c								*/
-/*												*/
-/*		Copyright (C) 1997, NINTENDO Co,Ltd.				*/
-/*												*/
+/*		NuSYS										                    */
+/*		nugfxthread.c								                    */
+/*												                        */
+/*		Copyright (C) 1997, NINTENDO Co,Ltd.				            */
+/*												                        */
 /*----------------------------------------------------------------------*/    
-/* Doesn't match Ver 1.0, 1.2, or 1.3 :(                        		*/
+/* Modified version 1.0; added nuGfxpending functionality.          	*/
 /*======================================================================*/
 #include <nusys.h>
 
-NUGfxFunc	nuGfxFunc = NULL;
+NUGfxFunc	    nuGfxFunc = NULL;
 NUGfxPreNMIFunc	nuGfxPreNMIFunc = NULL;
-OSMesgQueue	nuGfxMesgQ;
+OSMesgQueue	    nuGfxMesgQ;
 
 static OSMesg	nuGfxMesgBuf[NU_GFX_MESGS];
-static OSThread	GfxThread;			/* graphic thread */
-static u64	GfxStack[NU_GFX_STACK_SIZE/8];	/* gfx thread stack  */
+static OSThread	GfxThread;			            /* graphic thread */
+static u64	    GfxStack[NU_GFX_STACK_SIZE/8];	/* gfx thread stack  */
 
-static void nuGfxThread(void* arg);
+/*-----------------------------------------------------------------------*/
+/*	nuGfxThread - The graphic thread for the application 			     */
+/*	IN:	*arg	The argument of osCreateThread.  Nothing in particular   */
+/*	RET:	None										                 */
+/*-----------------------------------------------------------------------*/
+static void nuGfxThread(void *arg)
+{
+    u8          nuGfxpending;   // HM64: Added this variable and its handling.
+    NUScClient	gfx_client;
+    NUScMsg*	mesg_type;
+
+    nuGfxpending = 0;
+    nuGfxFunc = NULL;
+    nuGfxPreNMIFunc = NULL;
+    
+    osCreateMesgQueue(&nuGfxMesgQ, nuGfxMesgBuf, NU_GFX_MESGS);
+
+    /* Register as the RETRACE client to the scheduler */
+    nuScAddClient(&gfx_client, &nuGfxMesgQ, NU_SC_RETRACE_MSG | NU_SC_PRENMI_MSG);
+
+    while(1)
+    {
+	(void)osRecvMesg(&nuGfxMesgQ, (OSMesg*)&mesg_type, OS_MESG_BLOCK);
+    
+    switch(*mesg_type)
+    {
+	case NU_SC_RETRACE_MSG:	/* The retrace message */
+    {
+        if(!(nuGfxpending & 1))
+        {
+            /* Call the graphic function of the application side by the retrace message */
+    	    /* nuGfxpending is the interruption over-flag. */
+    	    if(nuGfxFunc != NULL)
+            {
+    		    (*nuGfxFunc)(nuGfxTaskSpool);
+    	    }
+
+            nuGfxpending = 0;
+        }
+        
+        nuGfxpending++;
+    }
+	break;
+	case NU_SC_PRENMI_MSG: /* The PRENMI message */
+    {
+        /* Call the call-back function in PRE NMI functions */
+	    if(nuGfxPreNMIFunc != NULL)
+        {
+		    (*nuGfxPreNMIFunc)();
+            
+	    }
+    }
+	break;
+	default:
+		break;
+	}
+    }
+}
 
 /*----------------------------------------------------------------------*/
 /*	nuGfxThreadStart - Activate the graphic thread 				*/
@@ -29,8 +85,6 @@ void nuGfxThreadStart(void)
 {
     /* Activate the graphic thread */
     osCreateThread(&GfxThread, NU_GFX_THREAD_ID, nuGfxThread, (void*)NULL,
-		   (GfxStack + NU_GFX_STACK_SIZE/8), NU_GFX_THREAD_PRI);
+		           (GfxStack + NU_GFX_STACK_SIZE/8), NU_GFX_THREAD_PRI);
     osStartThread(&GfxThread);
 }
-
-INCLUDE_ASM(const s32, "lib/nusys-1/nugfxthread", nuGfxThread);
