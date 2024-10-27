59b58c9cb5d2881f54e02a7277cd25a6a7be28af
cblck <156492894+cblck@users.noreply.github.com>
2024-10-26 21:43:34 -0700

    Updates pimgr.c data symbols.

--- src/lib/os/libultra/io/pimgr.c
+++ src/lib/os/libultra/io/pimgr.c
@@ -1,11 +1,23 @@
 #include "common.h"
+//
+#include "macros.h"
+#include "PR/os_internal.h"
+#include "PR/ultraerror.h"
+#include "PRinternal/piint.h"
+#include "PR/rdb.h"
+
+OSDevMgr __osPiDevMgr = { 0 };
+OSPiHandle* __osPiTable = NULL;
+#if BUILD_VERSION >= VERSION_J
+OSPiHandle __Dom1SpeedParam ALIGNED(8);
+OSPiHandle __Dom2SpeedParam ALIGNED(8);
+OSPiHandle* __osCurrentHandle[2] ALIGNED(8) = { &__Dom1SpeedParam, &__Dom2SpeedParam };
+#else
+extern OSPiHandle CartRomHandle;
+extern OSPiHandle LeoDiskHandle;
+OSPiHandle* __osCurrentHandle[2] ALIGNED(8) = { &CartRomHandle, &LeoDiskHandle };
+#endif
 
 INCLUDE_ASM(const s32, "lib/os/libultra/io/pimgr", osCreatePiManager);
 
 INCLUDE_ASM(const s32, "lib/os/libultra/io/pimgr", ramromMain);
-
-INCLUDE_RODATA(const s32, "lib/os/libultra/io/pimgr", __osPiDevMgr);
-
-INCLUDE_RODATA(const s32, "lib/os/libultra/io/pimgr", __osPiTable);
-
-INCLUDE_RODATA(const s32, "lib/os/libultra/io/pimgr", __osCurrentHandle);
