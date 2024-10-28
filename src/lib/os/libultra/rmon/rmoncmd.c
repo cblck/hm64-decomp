30eea35991d0c839c027eb2ff0b82dbbde417041
cblck <156492894+cblck@users.noreply.github.com>
2024-10-27 18:44:00 -0700

    Updates and renames rmoncmd.c to rmoncmds.c.

--- src/lib/os/libultra/rmon/rmoncmd.c
+++
@@ -1,8 +0,0 @@
-#include "common.h"
-
-
-INCLUDE_ASM(const s32, "lib/os/libultra/rmon/rmoncmd", __rmonExecute);
-
-INCLUDE_ASM(const s32, "lib/os/libultra/rmon/rmoncmd", NotImplemented);
-
-INCLUDE_RODATA(const s32, "lib/os/libultra/rmon/rmoncmd", dispatchTable);
